501e77474a9c9a27a460939a6b17ba29bdb89844
TolimiHorizontai <39697630+TolimiHorizontai@users.noreply.github.com>
2019-03-19 11:12:41 +0200

    pabaigti postai su kategorijomis

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -37,7 +37,7 @@ class AdminPostsController extends Controller
     {
         //
         $categories = Category::pluck('name', 'id')->all();
-        return view('admin.posts.create');
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
